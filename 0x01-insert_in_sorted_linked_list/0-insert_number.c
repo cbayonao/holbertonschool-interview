178aafd1c42deb543a9377d8d9920afcbdeed5fc
cbayonao <drcamilob@live.com>
2020-07-25 21:44:32 -0500

    [Add] header and function to insert in sorted linked list

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_node - Insert in sorted linked list
+ * @head: Double pointer to head
+ * @number: Integer with the number to insert
+ * Return: a listint_t with the address of the new node or NULL
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+	listint_t *prev;
+	listint_t *next;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = number;
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		*head = new;
+		return (new);
+	}
+	prev = *head;
+	next = prev->next;
+	while (prev)
+	{
+		if (prev->n >= number)
+		{
+			new->next = prev;
+			*head = new;
+			return (new);
+		}
+		if (prev->n <= number && next->n >= number)
+		{
+			prev->next = new;
+			new->next = next;
+			return (new);
+		}
+		if (!next && prev->n <= number)
+		{
+			prev->next = new;
+			new->next = NULL;
+			return (new);
+		}
+		prev = next;
+		next = prev->next;
+	}
+	return (NULL);
+}
