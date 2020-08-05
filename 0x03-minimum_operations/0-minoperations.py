1e798ab668eb8872d95e4ffe6f82af32f016ee6e
cbayonao <drcamilob@live.com>
2020-08-05 09:23:18 -0500

    [Add] File that find min Noperations

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Minimum Operations
+"""
+def minOperations(n):
+    """
+    Returns an integer
+    If n is impossible to achieve, return 0
+    """
+    nOperations = 0
+    for time in range(2, n+1):
+        while(n % time == 0):
+            nOperations += time
+            n = n/time
+    return(nOperations)
