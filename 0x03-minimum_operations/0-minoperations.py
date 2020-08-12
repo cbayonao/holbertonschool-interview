024c1875dfc5e50b096d56fd6f3aeacef0ceb660
cbayonao <drcamilob@live.com>
2020-08-12 03:48:00 +0000

    [fix] pep8 style

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -9,6 +9,8 @@ def minOperations(n):
     Returns an integer
     If n is impossible to achieve, return 0
     """
+    if not n or n < 2:
+        return 0
     nOperations = 0
     for time in range(2, n+1):
         while(n % time == 0):
