e2851fb1b039a5bdd6eababa47e0a7402cc62c30
cbayonao <drcamilob@live.com>
2020-09-14 21:03:10 +0000

    [Fix] Betty Style

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -1,10 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "sandpiles.h"
+/**
+ * estacopysec - make a secure copy of the grid
+ * @grid: integers grid to copy
+ * @copy: copy of the grid
+ */
 
 void estacopysec(int grid[3][3], int copy[3][3])
 {
 	int x, y;
+
 	for (x = 0; x < 3; x++)
 	{
 		for (y = 0; y < 3; y++)
@@ -13,7 +19,10 @@ void estacopysec(int grid[3][3], int copy[3][3])
 		}
 	}
 }
-
+/**
+ * print_grid - function to print grid
+ * @grid: grid to print
+ */
 static void print_grid(int grid[3][3])
 {
 	int i, j;
@@ -29,10 +38,14 @@ static void print_grid(int grid[3][3])
 		printf("\n");
 	}
 }
-
+/**
+ * cualquiera - function that make operations in neighbour
+ * @grid1: grid to do operations
+ */
 void cualquiera(int grid1[3][3])
 {
 	int x, y, valor = 0, copy[3][3];
+
 	estacopysec(grid1, copy);
 	for (x = 0; x < 3; x++)
 	{
@@ -43,21 +56,13 @@ void cualquiera(int grid1[3][3])
 				valor = 1;
 				grid1[x][y] -= 4;
 				if (x + 1 < 3)
-				{
 					grid1[x + 1][y] += 1;
-				}
 				if (x - 1 >= 0)
-				{
 					grid1[x - 1][y] += 1;
-				}
 				if (y + 1 < 3)
-				{
 					grid1[x][y + 1] += 1;
-				}
 				if (y - 1 >= 0)
-				{
 					grid1[x][y - 1] += 1;
-				}
 			}
 		}
 	}
@@ -68,10 +73,15 @@ void cualquiera(int grid1[3][3])
 		cualquiera(grid1);
 	}
 }
-
+/**
+ * sandpiles_sum - add the sandpiles
+ * @grid1: grid to add
+ * @grid2: grid to add
+ */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	int x, y;
+
 	for (x = 0; x < 3; x++)
 	{
 		for (y = 0; y < 3; y++)
