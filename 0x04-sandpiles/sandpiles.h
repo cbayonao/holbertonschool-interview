7587e962c25b6eaedf81f59907a69e4af5c202af
cbayonao <drcamilob@live.com>
2020-09-14 21:11:58 +0000

    [Fix] Header

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -2,5 +2,9 @@
 #define SANDPILES_H
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void estacopysec(int grid[3][3], int copy[3][3]);
+void cualquiera(int grid1[3][3]);
+
+
 
 #endif /* SANDPILES_H */
